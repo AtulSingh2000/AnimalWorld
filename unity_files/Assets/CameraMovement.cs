3f9791759ee42a968e762c20d3938632ca0213d5
Atul-Mages <atulkumar102s@gmail.com>
2022-01-17 23:16:43 +0530

    start scene setup

---
+++ unity_files/Assets/CameraMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    
+    public float speed = 20f;
+    public float scrollSpeed = 10f;
+
+    private Camera zoomCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoomCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(zoomCamera.orthographic)
+        {
+            zoomCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        }
+
+
+
+       Vector3 pos = transform.position;
+       
+       if(Input.GetKey("w"))
+       {  
+          pos.y += speed *Time.deltaTime;
+          Mathf.Clamp(pos.y, 0f, 29f);
+        }
+        if(Input.GetKey("s"))
+        {
+            
+            pos.y -= speed *Time.deltaTime;
+            Mathf.Clamp(pos.y, 0f, 29f);
+            
+        }
+        if(Input.GetKey("a"))
+        {
+            pos.x += speed *Time.deltaTime;
+            pos.z += speed *Time.deltaTime;
+        }
+        if(Input.GetKey("d"))
+        {
+            pos.x -= speed *Time.deltaTime;
+            pos.z -= speed *Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    
+    }
+
+    
+}
