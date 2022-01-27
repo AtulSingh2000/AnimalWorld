10e2468c58efc62e013ecc0bf2dec0f6bd0b3aef
Atul-Mages <atulkumar102s@gmail.com>
2022-01-27 18:51:33 +0530

    refined player movement

--- AnimalWorldGame/Assets/ThirdPersonMovement.cs
+++ AnimalWorldGame/Assets/ThirdPersonMovement.cs
@@ -11,7 +11,19 @@ public class ThirdPersonMovement : MonoBehaviour
     float turnSmoothVelocity;
 
     private Animator animator;
+
+    [Header("Gravity")]
+    public float gravity;
+    public float currentGravity;
+    public float maxGravity;
+    public float constantGravity;
+    private Vector3 gravityDirection;
+    private Vector3 gravityMovement;
     
+    private void Awake()
+    {
+        gravityDirection = Vector3.down;
+    }
     public void Start()
     {
         animator = GetComponent<Animator>();
@@ -19,6 +31,7 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateGravity();
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -31,10 +44,30 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized*speed *Time.deltaTime);
+            controller.Move(moveDir.normalized*speed *Time.deltaTime + gravityMovement);
         }
         else{
             animator.SetBool("Run Axe", false);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return controller.isGrounded;
+    }
+    private void CalculateGravity()
+    {
+        if(IsGrounded())
+        {
+            currentGravity = constantGravity;
+        }
+        else{
+            if(currentGravity > maxGravity)
+            {
+                currentGravity -= gravity * Time.deltaTime;
+            }
+        }
+
+        gravityMovement = gravityDirection * -currentGravity;
     }
 }
