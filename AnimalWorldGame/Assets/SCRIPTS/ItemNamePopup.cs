e430f2e806355a02aeaff289bca0607d27a94217
unknown <atulkumar102s@gmail.com>
2022-03-18 22:30:33 +0530

    labels and machine rework

--- AnimalWorldGame/Assets/SCRIPTS/ItemNamePopup.cs
+++ AnimalWorldGame/Assets/SCRIPTS/ItemNamePopup.cs
@@ -9,14 +9,26 @@ public class ItemNamePopup : MonoBehaviour
     public GameObject icecream;
     public GameObject bbq;
     public GameObject milk;
+    public GameObject popcorn;
+    public GameObject house;
+    public GameObject inventory;
+    public GameObject shop;
+    public GameObject tokenexchange;
+    
     // Start is called before the first frame update
     void Start()
     {
-         juicer.SetActive(false);
+        juicer.SetActive(false);
         bbq.SetActive(false);
         icecream.SetActive(false);
         milk.SetActive(false);
         feeder.SetActive(false);
+        popcorn.SetActive(false);
+        house.SetActive(false);
+        inventory.SetActive(false);
+        shop.SetActive(false);
+        tokenexchange.SetActive(false);
+        
 
     }
 
@@ -48,6 +60,28 @@ public class ItemNamePopup : MonoBehaviour
      {
          feeder.SetActive(true);
      }
+     if(gameObject.name == "PopcornMaker")
+     {
+         popcorn.SetActive(true);
+     }
+     if(gameObject.name == "House")
+     {
+         house.SetActive(true);
+     }
+     if(gameObject.name == "Inventory")
+     {
+         inventory.SetActive(true);
+     }
+     
+     if(gameObject.name == "Shop")
+     {
+         shop.SetActive(true);
+     }
+     if(gameObject.name == "TokenExchange")
+     {
+         tokenexchange.SetActive(true);
+     }
+     
     }
 
      private void OnMouseExit()
@@ -57,5 +91,10 @@ public class ItemNamePopup : MonoBehaviour
         icecream.SetActive(false);
         milk.SetActive(false);
         feeder.SetActive(false);
+        popcorn.SetActive(false);
+        house.SetActive(false);
+        inventory.SetActive(false);
+        shop.SetActive(false);
+        tokenexchange.SetActive(false);
     }
 }
